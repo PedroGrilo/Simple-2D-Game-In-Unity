6c2516a4caa33a487aa2d4c509532ee91a301bb8
PedroGrilo <xpereiragrilo@gmail.com>
2019-10-02 10:32:29 +0100

    dialog

--- Assets/ItemsToBuy.cs
+++ Assets/ItemsToBuy.cs
@@ -1,14 +1,16 @@
 ﻿    using System;
-    using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using TMPro;
+    using UnityEngine;
     using UnityEngine.UI;
 
     public class ItemsToBuy : MonoBehaviour{
         public int fireballCost;
         public int heartsCost;
         private int maxItems = 2;
-        public Text coins;
+        public Text coins; 
+
+        public GameObject Dialog;
+        public TextMeshProUGUI DialogText;
 
     // Start is called before the first frame update
     void Start()
@@ -48,7 +50,8 @@ using UnityEngine;
     }
 
     public void BuyItem(){
-        
+
+        bool activeDialog = false;
         bool fireball =  gameObject.transform.GetChild(0).gameObject.active;
         bool hearts = gameObject.transform.GetChild(1).gameObject.active;
 
@@ -56,9 +59,20 @@ using UnityEngine;
             PlayerStats.Coins -= fireballCost;
             PlayerStats.Fireball++;
         }
+        else{
+            activeDialog = true;
+        }
         if (hearts && Int32.Parse(coins.text)>=heartsCost){
             PlayerStats.Coins -= heartsCost;
             PlayerStats.Hearts++;
+        }
+        else{
+            activeDialog = true;
+        }
+       
+        if(activeDialog){
+            DialogText.text = "Enough money :(";
+            Dialog.SetActive(true);
         }
         
         coins.text = PlayerStats.Coins.ToString();
